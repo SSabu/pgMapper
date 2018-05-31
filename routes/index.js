a81513d12b3e97b237a8038b6465fdaba24bcd9b
SSabu <sandeepsabu09@gmail.com>
2018-05-30 18:03:41 -0700

    DB trial

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ const config = "postgres://sandeepsabu:Spi9dlee6@ssdbinstance.cs3dgspl0uzj.us-we
 
 const client = new pg.Client(config);
 
-const query = client.query("SELECT ST_AsGeoJSON(wkb_geometry) FROM sfparks;");
+var query = "SELECT ST_AsGeoJSON(wkb_geometry) FROM sfparks;";
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
@@ -26,14 +26,12 @@ app.get('/', function(req, res, next) {
 /* GET Postgres JSON data */
 
 app.get('/map', function(req, res, next) {
-  client.connect();
-
-    // function(err, done) {
-    // if (err) {
-    //   return console.error('error fetching client from pool', err);
-    // }
+  client.connect(function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
     console.log('connected to database');
-    query.on(function(err, result) {
+    client.query(query, function(err, result) {
       done();
       if (err) {
         return console.error('error running query', err);
@@ -41,29 +39,30 @@ app.get('/map', function(req, res, next) {
       res.render('map', {title:'Click on map to locate nearby parks. Click refresh to reload parks.', result:result});
     });
   });
-
-app.post('/map', function(req, res, next) {
-
-  var coordinates = req.body['lonlat[]'];
-
-  let lon = Number(coordinates[0]);
-  let lat = Number(coordinates[1]);
-
-  let nearestQuery = "SELECT ST_AsGeoJSON(wkb_geometry) FROM sfparks ORDER BY wkb_geometry <-> ST_SetSRID(ST_MakePoint("+lon+","+lat+"), 4326) LIMIT 5;";
-
-  pool.connect(function(err, client, done) {
-    if (err) {
-      return console.error('error fetching client from pool', err);
-    }
-    console.log('connected to database again');
-    client.query(nearestQuery, function(err, data) {
-      done();
-      if (err) {
-        return console.error('error running query', err);
-      }
-      res.send(data);
-    });
-  });
 });
+
+// app.post('/map', function(req, res, next) {
+//
+//   var coordinates = req.body['lonlat[]'];
+//
+//   let lon = Number(coordinates[0]);
+//   let lat = Number(coordinates[1]);
+//
+//   let nearestQuery = "SELECT ST_AsGeoJSON(wkb_geometry) FROM sfparks ORDER BY wkb_geometry <-> ST_SetSRID(ST_MakePoint("+lon+","+lat+"), 4326) LIMIT 5;";
+//
+//   pool.connect(function(err, client, done) {
+//     if (err) {
+//       return console.error('error fetching client from pool', err);
+//     }
+//     console.log('connected to database again');
+//     client.query(nearestQuery, function(err, data) {
+//       done();
+//       if (err) {
+//         return console.error('error running query', err);
+//       }
+//       res.send(data);
+//     });
+//   });
+// });
 
 module.exports = app;
