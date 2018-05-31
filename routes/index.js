00e3a02488ea57769b393ff83bffd92f299c889d
SSabu <sandeepsabu09@gmail.com>
2018-05-30 19:18:15 -0700

    DB working

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,8 @@ const pg = require('pg');
 const env = require('dotenv').load();
 const url = require('url');
 
-
-
-// var query = ;
-
 /* GET home page. */
+
 app.get('/', function(req, res, next) {
   res.render('index', { title: 'SF Park Finder' });
 });
@@ -21,13 +18,13 @@ app.get('/map', function(req, res, next) {
 
   var client = new pg.Client(config);
 
-  client.connect(function(err, client, done) {
+  client.connect(function(err, client) {
     if (err) {
       return console.error('error fetching client from pool', err);
     }
     console.log('connected to database');
-    client.query("SELECT ST_AsGeoJSON(wkb_geometry) FROM sfparks;", function(err, result) {
-      done();
+    client.query("SELECT ST_AsGeoJSON(wkb_geometry) FROM sfparks;", function(err, result, done) {
+      client.end();
       if (err) {
         return console.error('error running query', err);
       }
@@ -36,28 +33,32 @@ app.get('/map', function(req, res, next) {
   });
 });
 
-// app.post('/map', function(req, res, next) {
-//
-//   var coordinates = req.body['lonlat[]'];
-//
-//   let lon = Number(coordinates[0]);
-//   let lat = Number(coordinates[1]);
-//
-//   let nearestQuery = "SELECT ST_AsGeoJSON(wkb_geometry) FROM sfparks ORDER BY wkb_geometry <-> ST_SetSRID(ST_MakePoint("+lon+","+lat+"), 4326) LIMIT 5;";
-//
-//   pool.connect(function(err, client, done) {
-//     if (err) {
-//       return console.error('error fetching client from pool', err);
-//     }
-//     console.log('connected to database again');
-//     client.query(nearestQuery, function(err, data) {
-//       done();
-//       if (err) {
-//         return console.error('error running query', err);
-//       }
-//       res.send(data);
-//     });
-//   });
-// });
+app.post('/map', function(req, res, next) {
+
+  var coordinates = req.body['lonlat[]'];
+
+  let lon = Number(coordinates[0]);
+  let lat = Number(coordinates[1]);
+
+  var nearestQuery = "SELECT ST_AsGeoJSON(wkb_geometry) FROM sfparks ORDER BY wkb_geometry <-> ST_SetSRID(ST_MakePoint("+lon+","+lat+"), 4326) LIMIT 5;";
+
+  var config = "postgres://sandeepsabu:Spi9dlee6@ssdbinstance.cs3dgspl0uzj.us-west-1.rds.amazonaws.com:5432/sfparks";
+
+  var client = new pg.Client(config);
+
+  client.connect(function(err, client) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log('connected to database again');
+    client.query(nearestQuery, function(err, data) {
+      client.end();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(data);
+    });
+  });
+});
 
 module.exports = app;
